e818a98696d037d5c939d03f74e79ea20e2a9f3c
naporitan <naporin0624@gmail.com>
2018-06-04 20:18:24 +0900

    start

---
+++ src/main/RectangleFigure.java
@@ -0,0 +1,15 @@
+package main;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class RectangleFigure extends Figure {
+	  public RectangleFigure(int x,int y,int w,int h,Color c) {
+	    super(x,y,w,h,c);
+	    // 引数付きのコンストラクタは継承されないので，コンストラクタを定義．
+	    // superで親のコンストラクタを呼び出すだけ．
+	  }
+	  public void draw(Graphics g) {
+	    g.setColor(color);
+	    g.drawRect(x,y,width,height);
+	  }
+	}
